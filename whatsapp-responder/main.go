70b34f4c6b1db2283416ec4465973fed23f91cb9
odeandialamsyah <odeandialamsyah@gmail.com>
2025-04-30 09:28:24 +0700

    update main

--- whatsapp-responder/main.go
+++ whatsapp-responder/main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
+	// Connect ke MongoDB
 	err := ConnectDB()
 	if err != nil {
-		log.Fatalf("Koneksi MongoDB gagal: %v", err)
+		log.Fatalf("Gagal koneksi ke MongoDB: %v", err)
 	}
 
+	// Setup handler untuk webhook
 	http.HandleFunc("/webhook", webhookHandler)
 
+	// Jalankan server
 	fmt.Println("Server berjalan di port 8080 🚀")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
