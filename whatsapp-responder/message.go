0aa15cf3ace9fbfa63f7724f61ebe5df9f69b70d
odeandialamsyah <odeandialamsyah@gmail.com>
2025-04-30 09:27:53 +0700

    update struck model

--- whatsapp-responder/message.go
+++ whatsapp-responder/message.go
@@ -2,10 +2,14 @@ package main
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Message struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Number    string             `bson:"number"`
-	Message   string             `bson:"message"`
-	Direction string             `bson:"direction"` // "incoming" atau "outgoing"
-	Timestamp int64              `bson:"timestamp"`
+
+type Keyword struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	Keyword string            `bson:"keyword"`
+	Reply   string            `bson:"reply"`
+}
+
+type IncomingMessage struct {
+	Number  string `json:"number"`  // Nomor pengirim
+	Message string `json:"message"` // Isi pesan
 }
